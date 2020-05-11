7869d2ded64657eff862cacaafb974827632fa45
SeyfGoumeida <goumeidaahmedseyfeddine@gmail.com>
2020-05-11 02:17:01 +0100

    [FIX] UI

--- app/src/main/java/com/example/Dictionary_Audio_Images_Video_Usage/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/Dictionary_Audio_Images_Video_Usage/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Home Fragment"
+        value = "Tech Dictionary"
     }
     val text: LiveData<String> = _text
 
